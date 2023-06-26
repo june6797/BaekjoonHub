3a49f81ce173b92a857375e7b3851dfecfe483c8
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-06-26 12:01:23 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 264 ms, Memory: 99756 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+#define INF 1000000000
+int adj[5001][5001];
+int dist[5001];
+int visited[5001];
+
+int get_small_node(int N) {
+	int min = INF + 1;
+	int min_idx = 0;
+
+	for (int j = 1; j <= N; j++)
+	{
+		if (visited[j] == 0 && dist[j] < min)
+		{
+			min = dist[j];
+			min_idx = j;
+		}
+	}
+	return min_idx;
+}
+
+void dijkstra(int start, int N) {
+	visited[start] = 1;
+	dist[start] = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		dist[i] = adj[start][i];
+	}
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		int min_node = get_small_node(N);
+
+		visited[min_node] = 1;
+		for (int k = 1; k <= N; k++)
+		{
+			if (visited[k] == 0)
+			{
+				if (dist[k] > (dist[min_node] + adj[min_node][k]))
+				{
+					dist[k] = (dist[min_node] + adj[min_node][k]);
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	for (int i = 0; i < 5001; i++)
+	{
+		for (int j = 0; j < 5001; j++)
+		{
+			if (i == j)
+			{
+				adj[i][j] = 0;
+			}
+			else
+			{
+				adj[i][j] = INF;
+			}
+		}
+	}
+	for (int i = 0; i < 5001; i++)
+	{
+		dist[i] = INF;
+	}
+
+	int N, M, s, d, w, start, end;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> s >> d >> w; //s 시작도시 d 도착도시 w 비용
+
+		if (w < adj[s][d]) {
+			adj[s][d] = w;
+			adj[d][s] = w;
+		}
+	}
+	cin >> start >> end;
+
+	dijkstra(start, N);
+
+	cout << dist[end];
+
+}
