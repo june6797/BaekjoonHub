a8864257bc295bce24017e90c42160cf63104eb3
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-12 01:04:15 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	priority_queue<int> pq; //max_heap구현
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		if (num == 0) {
+			if (pq.size() == 0)
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(num);
+		}
+	}
+}
