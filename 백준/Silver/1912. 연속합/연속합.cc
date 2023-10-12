95cc76d5eae1140ddf6f789ad2c960b843ef2df9
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-12 12:42:42 +0900

    [Silver II] Title: 연속합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int dp[100001];
+
+int main() {
+	int n, num, ans = INT_MIN;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> num;
+		if (dp[i - 1] + num > 0)
+		{
+			dp[i] = dp[i - 1] + num;
+			ans = max(ans, dp[i - 1] + num);
+		}
+		else
+		{
+			dp[i] = 0;
+			ans = max(ans, dp[i - 1] + num);
+		}
+	}
+	cout << ans;
+}
