ba693dacb43f7a48aaa13d8beefa7febc8ee6e4a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-15 18:43:38 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[2][16];
+int dp[16];
+int main() {
+	int N, ans = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> arr[0][i];
+		cin >> arr[1][i];
+	}
+
+	for (int i = 1; i <= N + 1; i++)
+	{
+		dp[i] = max(dp[i], ans);
+		int f = i + arr[0][i];
+		if (f <= N + 1)
+		{
+			dp[f] = max(dp[f], arr[1][i] + dp[i]);
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
+}
