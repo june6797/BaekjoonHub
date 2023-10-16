a8666653cf1334305d63f8e09273408cc97745cc
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-16 20:12:40 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[10001];
+int dp[10001];
+
+int main() {
+	int n, ans = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	dp[1] = arr[1];
+	ans = dp[1];
+	dp[2] = arr[1] + arr[2];
+	ans = max(ans, dp[2]);
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i],
+			dp[i - 2] + arr[i]);
+		dp[i] = max(dp[i], dp[i - 1]);
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans << "\n";
+}
