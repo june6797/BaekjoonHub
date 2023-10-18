3396d08451f2b98c1ffb247e21f74eba47648b2a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-18 16:40:27 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#define b 1000000000
+using namespace std;
+
+int arr[101][10];
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i < 10; i++)
+	{
+		arr[1][i] = 1;
+	}
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				arr[i][j] = arr[i - 1][1] % b;
+			}
+			else if (j == 9)
+			{
+				arr[i][j] = arr[i - 1][8] % b;
+			}
+			else
+			{
+				arr[i][j] = arr[i - 1][j - 1] % b + arr[i - 1][j + 1] % b;
+			}
+		}
+	}
+	long ans = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		ans = (ans + arr[N][i]) % b;
+	}
+	cout << ans;
+}
