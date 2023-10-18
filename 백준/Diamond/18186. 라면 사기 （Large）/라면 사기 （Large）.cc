303abbb4388f95be9b0e9044eb14e94b832b3b0f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-19 00:38:13 +0900

    [Diamond IV] Title: 라면 사기 (Large), Time: 112 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+long long int arr[1000003];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long int N, B, C;
+	cin >> N >> B >> C;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	long long int ans = 0;
+
+	if (B < C)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			ans += arr[i];
+		}
+		ans = ans * B;
+	}
+	else
+	{
+
+		for (int i = 0; i < N; i++)
+		{
+			if (arr[i + 1] <= arr[i + 2])
+			{
+				int check = min(arr[i], arr[i + 1]);
+
+				ans += (B + 2 * C) * check;
+				arr[i] -= check;
+				arr[i + 1] -= check;
+				arr[i + 2] -= check;
+
+				if (arr[i] != 0)
+				{
+					ans += B * arr[i];
+					arr[i] = 0;
+				}
+			}
+			else
+			{
+				int check = min(arr[i], arr[i + 1] - arr[i + 2]);
+				ans += (B + C) * check;
+				arr[i] -= check;
+				arr[i + 1] -= check;
+
+				check = min(arr[i], arr[i + 2]);
+				ans += (B + 2 * C) * check;
+				arr[i] -= check;
+				arr[i + 1] -= check;
+				arr[i + 2] -= check;
+				if (arr[i] != 0)
+				{
+					ans += B * arr[i];
+					arr[i] = 0;
+				}
+			}
+		}
+	}
+	cout << ans;
+}
