48b50f724b7c76d4c1f18f7247e710e0b9471c9c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-22 10:50:20 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (i * 2) - 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = N - 1; i >= 1; i--) {
+        for (int j = 1; j <= N - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (i * 2) - 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
