74d3049255fccb1dd42f89b2d318fc74435cb42c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-10-26 16:19:31 +0900

    [Silver I] Title: 스티커, Time: 304 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int arr[2][100001];
+int dp[2][100001];
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int n;
+		cin >> n;
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				cin >> arr[i][j];
+			}
+		}
+		int ans = 0;
+		dp[0][1] = arr[0][1];
+		dp[1][1] = arr[1][1];
+		dp[0][2] = dp[1][1] + arr[0][2];
+		dp[1][2] = dp[0][1] + arr[1][2];
+
+
+		for (int i = 3; i <= n; i++)
+		{
+			dp[0][i] = max(dp[1][i - 1] + arr[0][i], dp[1][i - 2] + arr[0][i]);
+			dp[1][i] = max(dp[0][i - 1] + arr[1][i], dp[0][i - 2] + arr[1][i]);
+		}
+
+
+
+		cout << max(dp[0][n], dp[1][n]) << "\n";
+	}
+}
