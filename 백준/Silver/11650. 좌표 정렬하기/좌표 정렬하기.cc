2a5b29248251958c6ded62db656002abfe197c67
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-11-06 02:28:11 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 116 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	vector<pair<int, int>> v;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+}
