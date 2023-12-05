1d96454d8d379a320215b8890e2c2ca4a18ab383
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-06 00:34:59 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int arr[51];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int M, K, cnt = 0;
+	double ans = 0;
+	cin >> M;
+	for (int i = 1; i <= M; i++)
+	{
+		cin >> arr[i];
+		cnt += arr[i];
+	}
+	cin >> K;
+	for (int i = 1; i <= M; i++)
+	{
+		if (arr[i] >= K)
+		{
+			double temp = 1;
+			for (int j = 0; j < K; j++)
+			{
+				temp *= static_cast<double>(arr[i] - j) / (cnt - j);
+			}
+			ans += temp;
+		}
+	}
+
+	cout.precision(9);
+	cout << fixed;
+	cout << ans;
+}
