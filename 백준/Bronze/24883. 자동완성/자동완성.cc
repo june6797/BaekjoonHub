1741421a28b0f05be4b9bec9f4a65b71e9794b2d
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-28 00:39:39 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	char a;
+	cin >> a;
+	if (a == 'N' || a == 'n')
+	{
+		cout << "Naver D2";
+	}
+	else
+	{
+		cout << "Naver Whale";
+	}
+
+}
