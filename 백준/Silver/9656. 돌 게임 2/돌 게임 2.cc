8711683ace0abac09004373e7bfbeed1e2ae5ede
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-27 14:57:52 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int N;
+	cin >> N;
+	if (N % 2 == 0)
+	{
+		cout << "SK";
+	}
+	else
+	{
+		cout << "CY";
+	}
+}
