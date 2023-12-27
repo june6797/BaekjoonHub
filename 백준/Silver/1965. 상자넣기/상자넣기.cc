0d0323b56c8bbe9a076e205f499875d409f8374e
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-27 11:19:37 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int dp[1001];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+	cout << ans;
+}
