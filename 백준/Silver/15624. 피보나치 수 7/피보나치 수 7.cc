52d16dbcb7b7d13180e5848d8e0d1f6afbdcbf8a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-27 22:11:29 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 0 ms, Memory: 2020 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+long long int fib[2][2];
+long long int i[2][2];
+long long int temp[2][2];
+void mul(long long int arr[2][2], long long int arr2[2][2]) {
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			temp[i][j] = 0;
+			for (int k = 0; k < 2; k++)
+			{
+				temp[i][j] += (arr[i][k] * arr2[k][j]) % 1000000007;
+			}
+			temp[i][j] %= 1000000007;
+		}
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			arr[i][j] = temp[i][j];
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	fib[0][0] = 1;
+	fib[0][1] = 1;
+	fib[1][0] = 1;
+	fib[1][1] = 0;
+
+	i[0][0] = 1;
+	i[0][1] = 0;
+	i[1][0] = 0;
+	i[1][1] = 1;
+
+	long long int n;
+	cin >> n;
+	while (n > 0)
+	{
+		if (n % 2 == 1)
+		{
+			mul(i, fib);
+			n--;
+		}
+		else
+		{
+			mul(fib, fib);
+			n /= 2;
+		}
+	}
+	cout << i[0][1];
+}
