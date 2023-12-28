8c882b0d6d4acb5e54a9f49e011beda4b5067ff7
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-29 04:01:14 +0900

    [Silver II] Title: 점프 점프, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+int maze[1001];
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> maze[i];
+		dp[i] = 987654321;
+	}
+	dp[1] = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		int jump = maze[i];
+		for (int j = 1; j <= jump; j++)
+		{
+			if (i + j <= N)
+			{
+				dp[i + j] = min(dp[i + j], dp[i] + 1);
+			}
+		}
+	}
+	if (dp[N] != 987654321)
+	{
+		cout << dp[N];
+	}
+	else
+	{
+		cout << -1;
+	}
+}
