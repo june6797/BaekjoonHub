bc9e6dd7efad64d74c12be7056d30321e390313f
Kim Sung June <117192361+june6797@users.noreply.github.com>
2023-12-30 12:37:54 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int L[21];
 int J[21];
-int dp[21][101];
+int dp[101];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -20,18 +20,10 @@ int main() {
 	int ans = 0;
 	for (int i = 1; i <= N; i++)
 	{
-		for (int j = 0; j < 100; j++)
+		for (int j = 100; j >= L[i]; j--)
 		{
-			if (j - L[i] >= 0)
-			{
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - L[i]] + J[i]);
-			}
-			else
-			{
-				dp[i][j] = dp[i - 1][j];
-			}
-			ans = max(ans, dp[i][j]);
+			dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
 		}
 	}
-	cout << dp[N][99];
+	cout << dp[99];
 }
