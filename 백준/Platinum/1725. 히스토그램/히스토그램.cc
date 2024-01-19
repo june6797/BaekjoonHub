67d62326a07058eace3382400746f631ccb5400c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-01-20 03:07:12 +0900

    [Platinum V] Title: 히스토그램, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -1,34 +1,45 @@
 #include <iostream>
-#include <stack>
 using namespace std;
 
-int arr[100005];
-stack <int> s;
+long long int arr[100001];
 
+long long int solve(int s, int e) {
+	if (s == e)
+	{
+		return arr[s];
+	}
+	int mid = (s + e) / 2;
+	long long int ans = max(solve(s, mid), solve(mid + 1, e));
+	long long int left = mid;
+	long long int right = mid + 1;
+	long long int height = min(arr[left], arr[right]);
+	ans = max(ans, 2 * height);
+	while (s < left || right < e)
+	{
+		if (s < left && (right == e || arr[left - 1] > arr[right + 1]))
+		{
+			left--;
+			height = min(height, arr[left]);
+		}
+		else
+		{
+			right++;
+			height = min(height, arr[right]);
+		}
+		ans = max(ans, (right - left + 1) * height);
+	}
+	return ans;
+}
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int N;
-	int ans = 0;
-	s.push(0);
 	cin >> N;
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> arr[i];
 	}
-	for (int i = 1; i <= N + 1; i++)
-	{
-		while (!s.empty() && arr[s.top()] > arr[i])
-		{
-			int h = arr[s.top()];
-			s.pop();
-			int l = i - s.top() - 1;
-			ans = max(ans, h * l);
-		}
-		s.push(i);
-	}
-
-	cout << ans << "\n";
+	cout << solve(1, N) << "\n";
 
 }
