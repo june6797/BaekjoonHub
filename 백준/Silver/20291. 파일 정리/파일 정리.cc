7bd610266738dad4a514385332fb9431a7eabc8a
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-03-01 23:01:31 +0900

    [Silver III] Title: 파일 정리, Time: 180 ms, Memory: 21172 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -8,6 +8,9 @@ using namespace std;
 map<string, int> m;
 vector<string> v;
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 	int N;
 	cin >> N;
 	for (int i = 0; i < N; i++)
