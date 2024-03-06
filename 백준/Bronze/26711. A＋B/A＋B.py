71dd397060709f6d5820ff594a1ecf6d0b68427c
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-03-06 23:24:04 +0900

    [Bronze V] Title: A+B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26711. A＋B/A＋B.py
+++ 백준/Bronze/26711. A＋B/A＋B.py
@@ -1,9 +1,3 @@
-def A_plus_B(A, B):
-    return A + B
-
-
-if __name__ == "__main__":
-    A = int(input())
-    B = int(input())
-    
-    print(A_plus_B(A=A, B=B))
+a = int(input())
+b = int(input())
+print(a+b)
