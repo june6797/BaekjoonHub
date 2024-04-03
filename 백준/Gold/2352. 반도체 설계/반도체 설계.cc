e88e25d44a828b25e2f9e4dcc3f04ccb6c6713b7
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-04-04 01:19:22 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <queue>
+#include <map>
+typedef long long int ll;
+using namespace std;
+
+vector<int> v;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		if (v.size() == 0)
+		{
+			v.push_back(num);
+		}
+		else
+		{
+			if (v[v.size() - 1] < num)
+			{
+				v.push_back(num);
+			}
+			else
+			{
+				int start = 0;
+				int end = v.size() - 1;
+				while (start < end)
+				{
+					int mid = (start + end) / 2;
+					if (v[mid] < num)
+					{
+						start = mid + 1;
+					}
+					else
+					{
+						end = mid;
+					}
+				}
+				v[start] = num;
+			}
+		}
+	}
+	cout << v.size();
+}
