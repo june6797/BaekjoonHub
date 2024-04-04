257f028a9eb6c6555a0fdcfa9915a418d08df831
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-04-04 21:15:18 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <queue>
+#include <map>
+typedef long long int ll;
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
