f3dc0b8bd0d37b6c8dc09e672930cd944f14bedf
Kim Sung June <117192361+june6797@users.noreply.github.com>
2024-04-15 18:48:41 +0900

    [Gold V] Title: 줄어드는 수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <string>
+typedef long long int ll;
+using namespace std;
+
+int N;
+vector<ll> v;
+
+void solve(ll num) {
+	v.push_back(num);
+	int last = num % 10;
+	for (int i = 0; i < last; i++)
+	{
+		solve(10 * num + i);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N;
+	for (int i = 0; i < 10; i++)
+	{
+		solve(i);
+	}
+	sort(v.begin(), v.end());
+	if (v.size() < N)
+	{
+		cout << -1;
+	}
+	else
+	{
+		cout << v[N - 1];
+	}
+}
