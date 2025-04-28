91d19a4b348cc47610793a3cff8b29e289674ef6
Kim Sung June <117192361+june6797@users.noreply.github.com>
2025-04-28 09:35:05 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    if(ineq == "<") {
+        if(eq == "!") {
+            if(n < m) {
+                return 1;
+            }
+            else  {
+                return 0;
+            }
+        }
+        else {
+             if(n <= m) {
+                return 1;
+            }
+            else  {
+                return 0;
+            }
+        }
+    }
+    else {
+        if(eq == "!") {
+            if(n > m) {
+                return 1;
+            }
+            else  {
+                return 0;
+            }
+        }
+        else {
+            if(n >= m) {
+                return 1;
+            }
+            else  {
+                return 0;
+            }
+        }
+    }
+    return answer;
+}
