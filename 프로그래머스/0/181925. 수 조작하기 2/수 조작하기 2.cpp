4cc57d83f72b05cf2988261ae32e936b4c6fe0b0
Kim Sung June <117192361+june6797@users.noreply.github.com>
2025-04-28 11:50:20 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.39 ms, Memory: 6.77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for (int i = 1; i < numLog.size(); i++)
+{
+	if (numLog[i] - numLog[i - 1] == 1) {
+		answer += "w";
+	}
+	else if (numLog[i] - numLog[i - 1]== 10) {
+		answer += "d";
+	}
+	else if (numLog[i] - numLog[i - 1]== -10) {
+		answer += "a";
+	}
+	else
+	{
+		answer += "s";
+	}
+}
+    return answer;
+}
