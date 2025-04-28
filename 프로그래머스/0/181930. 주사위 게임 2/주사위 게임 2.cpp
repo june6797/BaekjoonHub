670db8f0b2f3d631480879b956041d2059ed9513
Kim Sung June <117192361+june6797@users.noreply.github.com>
2025-04-28 11:12:31 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    if(a == b && b ==c) {
+        
+        return(a + b + c) * (a * a + b * b +c* c) * (a*a*a + b*b*b + c*c*c);
+    }
+    else if( a== b) {
+        
+        return(a + b + c) * (a * a + b * b +c* c);
+    }
+    else if( a== c) {
+        
+        return(a + b + c) * (a * a + b * b +c* c);
+    }
+    else if(b ==c) {
+        return(a + b + c) * (a * a + b * b +c* c);
+    }
+    else {
+        return a + b +c;
+    }
+    return answer;
+}
