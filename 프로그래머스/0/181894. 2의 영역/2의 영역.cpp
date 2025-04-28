c26cdc08b692e0240cf26972e4619de5dcbedd30
Kim Sung June <117192361+june6797@users.noreply.github.com>
2025-04-28 16:21:52 +0900

    [level 0] Title: 2의 영역, Time: 25.30 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int s = -1;
+    int e = -1;
+    for(int i = 0; i <arr.size(); i++) {
+        if(arr[i] == 2) {
+            if(s == -1) {
+                s = i;
+                e= i;
+            }
+            else{
+                e= i;
+            }
+        }
+    }
+    if(s == -1) {
+        answer.push_back(-1);
+    }
+    else {
+    for(int i = s; i <= e; i++) {
+        answer.push_back(arr[i]);
+    }
+    }
+    return answer;
+}
